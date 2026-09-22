b30ac6cba9290ed477520a4c51c79cd5780b21bb
brasil.arelsa <brasil.arelsa@gmail.com>
2026-09-22 16:52:54 -0300

    docs: adiciona Swagger e cobertura no README

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,8 @@ module.exports = {
     "src/**/*.ts",
     "!src/server.ts",
     "!src/messaging/rabbitmq.ts",
-    "!src/database/connection.ts"
+    "!src/database/connection.ts",
+    "!src/swagger.ts"
   ],
   coverageThreshold: {
     global: { statements: 80, branches: 50, functions: 80, lines: 80 }
